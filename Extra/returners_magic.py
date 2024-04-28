48d8da351356c8812b58002798bf158399df338f
gurjot06oct <gurjot06oct@gmail.com>
2024-04-28 22:46:34 +0530

    updating

--- Extra/returners_magic.py
+++ Extra/returners_magic.py
@@ -1,7 +1,9 @@
 # calling fruitless function and returning value in list comprehension
 def fruitless(x):
     print(x)
-    return None
+    # It can return anything true, false, none etc
+    return True
 listing =  [1,2,3,4,5,6,7]
-listing2= [str(type(fruitless(i))) and i  for i in listing ]
-print(listing2)
+listing2 = [str(type(fruitless(i))) and i  for i in listing ]
+listing3 = [(fruitless(i) or True) and i  for i in listing ]
+print(listing2,listing3)
