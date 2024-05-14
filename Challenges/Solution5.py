3aac3e49ccae5c4024e86cfd16730fce91bcecda
gurjot06oct <gurjot06oct@gmail.com>
2024-05-14 16:34:11 +0530

    updates

--- Challenges/Solution5.py
+++ Challenges/Solution5.py
@@ -2,6 +2,6 @@ n = int(input("Enter the value of n: "))
 first, second, temp = 0, 1, 0
 
 
-fibonacci_series = [i if i < 2 else (temp:=first or True) and (first:=second or True) and (second:=temp+second or True) and second-temp for i in range(n+1)]
+fibonacci_series = [i if i < 2 else (temp:=first or True) and (first:=second) and (second:=temp+second) and second-temp for i in range(n+1)]
 
 print(fibonacci_series)
